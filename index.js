e9680ec4d62494c02da3c31de68211b21b8648db
Mitzi17 <m_estrada17@live.com>
2021-03-14 22:08:23 -0700

    commit tree 6c3f8b87f7a9e0767fe33022ec84000b0ccfb748

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrayNames, eventName) {
+    let thankYouCards = []
+    for (let i = 0; i < arrayNames.length; i++) {
+        thankYouCards.push(`Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num)
+}
